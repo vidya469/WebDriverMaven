2e9653c7190877bef52cbbb64d8391c1572f05ee
vidya <vidyarayudu668@gmail.com>
2020-01-09 03:23:02 +0530

    Intitial Commit

---
+++ src/test/java/mavenexamplegit/TestLogin.java
@@ -0,0 +1,34 @@
+package mavenexamplegit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestLogin {
+	
+public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://www.gmail.com");
+		driver.findElement(By.name("identifier")).sendKeys("vidyarayudu668@gmail.com");
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
